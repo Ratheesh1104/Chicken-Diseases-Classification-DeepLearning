c6c57efc2e6533611c28b62495dc26f49a6e0fd1
Ratheesh Mano <142888009+RatheeshMano@users.noreply.github.com>
2025-04-20 11:51:25 +0530

    update ingestion file

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+import sys
+print(sys.path)
 
-logger.info("Welcome to my custom log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<")
+    data_ingestion_pipeline = DataIngestionPipeline()
+    data_ingestion_pipeline.main()
+    logger.info(f">>>>>>>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
